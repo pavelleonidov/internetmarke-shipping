cdf405761f225a4241608287bc8b134bc4e9212a
Pavel Leonidov <pavelleonidov@gmail.com>
2017-11-19 17:03:09 +0100

    Initial commit

---
+++ WaWiApplication/src/main/java/com/allaboutapple/WaWi/WaWiApplication/WaWiApplication.java
@@ -0,0 +1,13 @@
+package com.allaboutapple.WaWi.WaWiApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+WaWiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaWiApplication.class, args);
+	}
+}
