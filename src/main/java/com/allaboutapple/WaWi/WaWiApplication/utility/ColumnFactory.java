68b2fd2c628d0e749bf54236487896164ebeeef7
Pavel Leonidov <pavelleonidov@gmail.com>
2018-01-02 16:17:14 +0100

    Helper stuff

--- src/main/java/com/allaboutapple/WaWi/WaWiApplication/utility/ColumnFactory.java
+++ src/main/java/com/allaboutapple/WaWi/WaWiApplication/utility/ColumnFactory.java
@@ -2,8 +2,12 @@ package com.allaboutapple.WaWi.WaWiApplication.utility;
 
 import com.allaboutapple.WaWi.WaWiApplication.model.magento.TreeObject;
 import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.cells.editors.base.JFXTreeTableCell;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 public class ColumnFactory<T extends TreeObject> {
@@ -22,6 +26,28 @@ public class ColumnFactory<T extends TreeObject> {
             @Override
             public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<T, String> orderTreeObjectStringCellDataFeatures) {
                 return orderTreeObjectStringCellDataFeatures.getValue().getValue().getProperty(property);
+            }
+
+
+        });
+
+        column.setCellFactory(new Callback<TreeTableColumn<T, String>, TreeTableCell<T, String>>() {
+            @Override
+            public TreeTableCell<T, String> call(TreeTableColumn<T, String> param) {
+                return new JFXTreeTableCell<T, String>() {
+                    private Text text;
+
+                    @Override
+                    public void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if(!isEmpty()) {
+                            text = new Text(item);
+                            text.setWrappingWidth(column.getWidth());
+                            this.setWrapText(true);
+                            setGraphic(text);
+                        }
+                    }
+                };
             }
         });
 
