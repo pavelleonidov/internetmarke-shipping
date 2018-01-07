91a8f521d73b99d785a6a406f20f9adc15cb9662
Pavel Leonidov <pavelleonidov@gmail.com>
2018-01-07 17:28:01 +0100

    Customized splash screen

---
+++ src/main/java/de/pavelleonidov/InternetmarkeShipping/SplashScreen.java
@@ -0,0 +1,8 @@
+package de.pavelleonidov.InternetmarkeShipping;
+
+public class SplashScreen extends de.felixroske.jfxsupport.SplashScreen {
+
+    public String getImagePath() {
+        return "/splash/Splash.png";
+    }
+}
